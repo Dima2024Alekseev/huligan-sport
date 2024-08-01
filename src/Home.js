3ca2953ed0d1c9e9b367fa5f0a133668b3ad905a
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-08-01 17:10:21 +0700

    done

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style/style.css"
 import Header from "./Components/Header";
 import News_Block from "./Components/News-Block";
 import Store_Block from "./Components/Store-Block";
