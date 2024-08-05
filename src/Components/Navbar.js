37a69f59d48136d030d02869eaf758e7ecd9f8b5
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-08-05 23:46:55 +0700

    done

---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul class="navigation">
+                <li><Link to="/home">Академия</Link>
+                    <ul>
+                        <li><Link to="/about">об Академии</Link></li>
+                        <li><Link to="/contact">Контакты</Link></li>
+                    </ul>
+                </li>
+                <li><a href="#">Направления</a>
+                    <ul>
+                        <li><a href="#">ММА</a></li>
+                        <li><a href="#">Грэпплинг</a></li>
+                        <li><a href="#">Бокс</a></li>
+                        <li><a href="#">Кикбоксинг</a></li>
+                        <li><a href="#">Рукопашный бой</a></li>
+                        <li><a href="#">Каратэ</a></li>
+                        <li><a href="#">Женская самооборона</a></li>
+                    </ul>
+                </li>
+                <li><Link to="/schedule">Расписание</Link></li>
+                <li><Link to="/events">События</Link></li>
+                <li><a href="#">Пресс-центр</a></li>
+                <li><a href="#">Интернет-магазин</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
