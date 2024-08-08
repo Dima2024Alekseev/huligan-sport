78da84f0b563a6a13a5eff7429891efd198a56c8
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-08-08 14:46:56 +0700

    done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 5000;
 const myCache = new NodeCache({ stdTTL: 300 }); // Кэширование данных на 5 минут
 
-const ACCESS_TOKEN = 'vk1.a.nfKxegYTWlNG2h3IYzPTP14vzl8uHlTwCYDuV5UhawZOCmurpKdalIwt7S5ytObFIYBvhM4uJsPjjBRe_lKdzgaWcisHWd2IZHC_-QYBZeR-OWFVGFb7Hu3WlGCdOCJFnnK1jcZsYyo5oFj5GI5qbjfnUa5S-5ZQDi7FumjdUQwwbjxiiCNA0xvuOz2NmBcT7_IfnUt4YvFAxVpKJLe3fw';
+const ACCESS_TOKEN = 'vk1.a.JRi3rpmUhtiZ8X3cPmNGdiwy3C7xhbHSI76u110imU2VsgD99A-C1bkAtC5Xs1V9-KAn-armltwY4qcc8crO-5YXZIKwBpskmG9kjV7iBLMRwXC_lRCTH9tToNVo81AdDgPC839c3W6pZKvh1MIF_Uff-G88i_TISVdZbDHzRa-FKpgoWx6v2G1o4SWXS0nhI7UHFyXyX0K8rFkaCNh4JA';
 const GROUP_ID = '216523190';
 
 app.use(cors());
