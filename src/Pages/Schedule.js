0fd725712af7cf5fb2743848c649cf0fdd32063c
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-08-19 15:39:51 +0700

    done

--- src/Pages/Schedule.js
+++ src/Pages/Schedule.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../style/shedule.css";
+import "../style/schedule.css";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
