90e6eceb9a65336f2369de3e029356abba19c582
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-08-25 15:01:01 +0700

    done

--- src/Components/Contact-Information.js
+++ src/Components/Contact-Information.js
@@ -18,7 +18,7 @@ class Contact extends React.Component {
               <h2 className="contact-title">Контакты</h2>
               <p className="contact-number-club">+7 (999) 445-12-03</p>
               <p className="address">г. Канск, улица 40 лет Октября, 62 ст 4, 2 этаж</p>
-              <h2 className="schedule">Режим работы Академии</h2>
+              <h2 className="operating-mode">Режим работы Академии</h2>
               <p className="mon-schedule">Понедельник - Cуббота: с 7.00 до 24.00</p>
               <p className="sun-schedule">Воскресенье: с 9.00 до 22.00</p>
             </div>
