7fb1dc5a5825ad3a98d74bc380d2f18cc9ecc425
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-11-19 11:27:05 +0700

    done

--- client/src/Pages/Directions/Karate.jsx
+++ client/src/Pages/Directions/Karate.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer/Footer";
-import karate_video from "../../video/karate.mp4";
+import karate_video from "../../video/karate.MOV";
 
 const Karate = () => {
     return (
