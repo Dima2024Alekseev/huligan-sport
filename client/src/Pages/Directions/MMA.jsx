373328b4b4a2b28181f91d2e418928e7896fd532
Dima2024Alekseev <alekseevdimas873@gmail.com>
2024-11-19 11:35:46 +0700

    done

--- client/src/Pages/Directions/MMA.jsx
+++ client/src/Pages/Directions/MMA.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer/Footer";
-import mmaVideo from "../../video/mma_test.mp4";
+import mmaVideo from "../../video/mma.mp4";
 
 const Mma = () => {
     return (
